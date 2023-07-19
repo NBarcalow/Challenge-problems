c677fb8319080ff2285121bd84ffc9abb9c9628b
NBarcalow <60424656+NBarcalow@users.noreply.github.com>
2023-07-19 12:21:09 -0600

    Update ElevatorProgram.py

--- Elevator/ElevatorProgram.py
+++ Elevator/ElevatorProgram.py
@@ -1,18 +1,7 @@
 import ElevatorObject
 import ElevatorCalls
 
-''' 
-    Evevator Program:
-        Features not added: 
-            That was not added was an event class to handle dynamically added buttons and calls to the lists
-            while the elevator is running. 
-            I did not add this, because I'm less familiar with python events, however, this is something I would
-            like to build on and learn more about.
 
-        Assumptions:
-            That the user will know the states of the elevator before calling changeElevatorState ( idle, up, down )
-
-'''
 
 def main():
     ElevatorCallList = ElevatorCalls.ElevatorCalls()
